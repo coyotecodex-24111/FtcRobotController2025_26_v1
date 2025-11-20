1c643f9f3264fc35f7753f11dbf56cd3b0c3bfc2
coyote-codex <coyotecodex@crsdhs.org>
2025-11-20 12:05:49 -0800

    Commit after deleting spin code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModeBasicV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoModeBasicV1.java
@@ -33,54 +33,11 @@ public class AutoModeBasicV1 extends LinearOpMode {
         waitForStart();
 
         if (opModeIsActive()) {
-            spin(LEFT, 10000);
-            sleep(1000);
-            spin(RIGHT, 5000);
+            moveRobot(4,0,0,0.5,2000);
         }
     }
 
-    private void spin(Direction direction, long milliSeconds) {
-        int speedPolarityLeft = direction == LEFT ? -1 : 1;
-        int speedPolarityRight = direction == LEFT ? 1 : -1;
 
-        double max;
-
-        double leftFrontPower = spinSpeed * speedPolarityLeft;
-        double rightFrontPower = spinSpeed * speedPolarityRight;
-        double leftBackPower = spinSpeed * speedPolarityLeft;
-        double rightBackPower = spinSpeed * speedPolarityRight;
-
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
-        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
-
-        if (max > 1) {
-            leftFrontPower /= max;
-            rightFrontPower /= max;
-            leftBackPower /= max;
-            rightBackPower /= max;
-
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status (Version: " + VERSION + ")", "Run Time: " + runtime);
-        telemetry.addData("About to spin: " + direction + " for " + milliSeconds + "ms", "Run Time: " + runtime);
-        telemetry.addData("About to spin: Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-        telemetry.addData("About to spin: Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-        telemetry.update();
-
-        robot.leftFrontDrive.setPower(leftFrontPower);
-        robot.rightFrontDrive.setPower(rightFrontPower);
-        robot.leftBackDrive.setPower(leftBackPower);
-        robot.rightBackDrive.setPower(rightBackPower);
-
-
-
-
-            sleep(milliSeconds);
-        robot.stopAllDriveMotors();
-    }
         public void moveRobot(int forward, int strafe, int rotate, double speed, int sleep) {
             final double FORWARD_RATIO = (100 / 59.0);
             final double SIDE_RATIO = (100 / 50.875);
